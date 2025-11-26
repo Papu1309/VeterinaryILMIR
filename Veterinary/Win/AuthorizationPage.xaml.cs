cf49eb8784f12b226d78d77511b2a9b40942549d
ilmir <ilmir@DESKTOP-9T3VNM7>
2025-11-26 12:17:31 +0300

    Добавьте файлы проекта.

---
+++ Veterinary/Win/AuthorizationPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Veterinary.Connection;
+using Veterinary.Properties;
+
+namespace Veterinary.Win
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        //DB.User u = new DB.User();
+
+        public User user {  get; set; }
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVhodd_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterUser(loginTxb.Text, parolTxb.Password);
+            NavigationService.Navigate(new UserPage(user));
+        }
+       
+        private void RegisterUser(string email, string password)
+        {
+            User users = new User();
+            users.login = email;
+            users.password = password;
+            DB.vet.User.Add(users);
+            DB.vet.SaveChanges();
+        }
+    }
+}
---
+++ Veterinary/Win/RegistrationPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Veterinary.Connection;
+
+
+namespace Veterinary.Win
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        public static ObservableCollection<User> sotrudniks { get; set; }
+        public static User AuthorisationSotr(string login, string password)
+        {
+            sotrudniks = new ObservableCollection<User>(DB.vet.User.ToList());
+            var userExists = sotrudniks.Where(sotrudniks => sotrudniks.login == login && sotrudniks.password == password).FirstOrDefault();
+            if (userExists != null)
+            {
+                return userExists;
+            }
+            else
+            {
+                return userExists;
+            }
+        }
+        public static User user;
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+        public User sotrudnik { get; private set; }
+
+        private void btnVhod_Click(object sender, RoutedEventArgs e)
+        {
+            string login = loginTxb.Text.Trim();
+            string password = parolTxb.Password.Trim();
+            user = AuthorisationSotr(login, password);
+            sotrudnik = AuthorisationSotr(login, password);
+            if (sotrudnik != null)
+            {
+                NavigationService.Navigate(new UserPage(user));
+
+            }
+            else MessageBox.Show("Логин или пароль неверный", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void btnVhod_Копировать_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AuthorizationPage());
+        }
+
+    }
+}
